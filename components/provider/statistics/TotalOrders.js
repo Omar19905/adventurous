91479def332a60c9e7595686bd79b8fd8b5d0cf4
Omar19905 <73073955+Omar19905@users.noreply.github.com>
2022-04-20 08:13:02 +0300

    loading for report

--- components/provider/statistics/TotalOrders.js
+++ components/provider/statistics/TotalOrders.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {Stat, StatArrow, StatHelpText, StatLabel, StatNumber} from "@chakra-ui/react";
+import {Spinner, Stat, StatArrow, StatHelpText, StatLabel, StatNumber} from "@chakra-ui/react";
 
 const TotalOrders = ({orders}) => {
+
     return (
         <Stat rounded={"lg"} textAlign={"center"} p={4} bg={"gray.50"} >
             <StatLabel color={"g.1"} fontSize={"lg"} fontWeight={"semibold"}>Total Orders</StatLabel>
             <StatNumber color={"g.2"}>{orders}</StatNumber>
+            {!orders&&<Spinner color={"g.2"} />}
+
             <StatHelpText>
                 {/*<StatArrow type='increase' />*/}
                 {/*23.35%*/}
--- components/provider/statistics/TotalSales.js
+++ components/provider/statistics/TotalSales.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Box, Stat, StatArrow, StatHelpText, StatLabel, StatNumber} from "@chakra-ui/react";
+import {Box, Spinner, Stat, StatArrow, StatHelpText, StatLabel, StatNumber} from "@chakra-ui/react";
 
 const TotalSales = ({sales}) => {
+
     return (
         <Stat rounded={"lg"}  textAlign={"center"} p={4} bg={"gray.50"} >
             <StatLabel fontSize={"lg"} color={"g.1"} fontWeight={"semibold"}>Total Sales</StatLabel>
-            <StatNumber color={"g.2"}>SAR{sales}</StatNumber>
+            {sales&&<StatNumber color={"g.2"}>
+                SAR{sales}
+            </StatNumber>}
+            {!sales&&<Spinner color={"g.2"} />}
             <StatHelpText>
                 {/*<StatArrow type='increase' />*/}
                 {/*10%*/}
